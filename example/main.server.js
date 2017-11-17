3281c0498df3f3b24a4b311232ded0e7f18b2d17
BlitzBanana <victor.rebiardcrepin@gmail.com>
2017-11-17 16:46:44 +0100

    Example

--- example/main.server.js
+++ example/main.server.js
@@ -1,20 +1,65 @@
+const Promise = require('bluebird')
 const Vantage = require('vantage')
 const figlet = require('figlet')
 const chalk = require('chalk')
+const _ = require('lodash')
 const repl = require('../dist')
+
+const users = {
+  1: {
+    id: 1,
+    account_id: 4,
+    shortid: 'B1-JRnYyM',
+    first_name: 'John',
+    last_name: 'Doe',
+    created_at: '2017-11-15T13:15:05.035Z',
+    updated_at: '2017-11-15T13:15:05.035Z'
+  },
+  2: {
+    id: 2,
+    account_id: 5,
+    shortid: 'A39cZzsDd',
+    first_name: 'Jane',
+    last_name: 'Doe',
+    created_at: '2017-12-15T13:15:05.035Z',
+    updated_at: '2017-11-17T09:12:28.230Z'
+  }
+}
 
 const infos = {
   name: 'main-server',
   version: '1.0.1'
 }
 
-const foobar = input => JSON.stringify(input)
+let prevCount = 50
+
+const app = {
+  requests: {
+    count() {
+      prevCount = _.random(prevCount + 1, prevCount + _.random(10, 100))
+      return prevCount
+    }
+  }
+}
+
+const db = {
+  users() {
+    return Promise.resolve(Object.values(users)).delay(1000)
+  },
+  user(id) {
+    if (users[id]) {
+      return Promise.resolve(users[id]).delay(1000)
+    }
+    return Promise.reject(new Error(`User ${id} not found.`)).delay(500)
+  }
+}
 
 new Vantage()
   .use(repl, {
     delimiter: chalk.red('repl:'),
-    description: 'Custom REPL mode !',
-    context: { infos, foobar },
+    description: 'Server REPL with access to app and db objects !',
+    banner: 'Have fun !',
+    context: { infos, app, db },
     formatter: 'highlight'
   })
   .banner(figlet.textSync('MAIN SERVER'))
